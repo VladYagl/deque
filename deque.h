1a4ea65163b79ad81161081a124a83b3b1321f62
Vlad <vlad082006@yandex.ru>
2016-06-19 14:41:41 +0300

    INIT3

--- deque.h
+++ deque.h
@@ -1,6 +1,35 @@
 #ifndef DEQUE_H
 #define DEQUE_H
 
+#include <string>
 
+struct deque {
+	struct iterator {
+		
+	};
+
+	struct const_iterator {
+		
+	};
+
+	deque();
+	deque(deque const& other);
+	deque& operator=(deque const& other);
+
+	size_t size() const;
+	bool empty() const;
+	void clear();
+
+	std::string& back();
+	std::string const& back() const;
+
+	void pop_back();
+	void pop_front();
+	void push_back(std::string const& a);
+	void push_front(std::string const& a);
+
+	std::string& operator[](size_t index);
+	std::string const& operator[](size_t index) const;
+};
 
 #endif // DEQUE_H
